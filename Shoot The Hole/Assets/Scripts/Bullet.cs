fb22225707eaa192a6799e13da2b5273b0da480e
Harry2166 <harry2166.hq@gmail.com>
2022-09-01 15:48:14 +0800

    Add mouse pointer

--- Shoot The Hole/Assets/Scripts/Bullet.cs
+++ Shoot The Hole/Assets/Scripts/Bullet.cs
@@ -4,27 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float speed = 20f;
+    //[SerializeField] private float speed = 20f;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private GameObject impactEffect;
     //[SerializeField] private AudioSource bullet_hit_sound_effect;
     // Start is called before the first frame update
-    void Start()
-    {
-        rb.velocity = transform.right * speed;
-    }
 
     void Update()
     {
-        if (transform.position.y > 5.5f)
+        if (transform.position.y > 5.5f || transform.position.y < -5.5f)
         {
             Destroy(this.gameObject);
         }
     }
-    void OnTriggerEnter2D()
+    void OnCollisionEnter2D(Collision2D collision)
     {
         //bullet_hit_sound_effect.Play();
-        Instantiate(impactEffect, transform.position, transform.rotation);
+        GameObject effect = Instantiate(impactEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
         Destroy(gameObject);
     }
 }
--- Shoot The Hole/Assets/Scripts/Weapon.cs
+++ Shoot The Hole/Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public float bulletForce = 20f;
     //public int bulletCount = 5;
     //private int countBullet = 5;
     //public bool isCollected = false;
@@ -20,7 +21,9 @@ public class Weapon : MonoBehaviour
 
     }
     void Shoot(){
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up*bulletForce, ForceMode2D.Impulse);
         //bulletCount -= 1;
     }
 
